f8c58c6e374a06907fd5840f582ea2f5d0964ddc
Sachin <sachinw68@gmail.com>
2020-08-09 17:38:02 +0530

    adding to github pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: "https://financialmodelingprep.com/api/v3/",
-  apiKey: "0396829bb45a9de8ec9db6183f7b0dc7" ,
-  //apiKey: "demo",
+  //apiKey: "0396829bb45a9de8ec9db6183f7b0dc7" ,
+  apiKey: "demo",
 };
 
 /*
